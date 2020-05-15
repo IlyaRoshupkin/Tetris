c5ecb93ab04f785f91bf93d3bef1d19ecb4d8b8f
IlyaRoshupkin <roshupkin.ilya@gmail.com>
2020-05-15 10:10:57 +0300

    Created the project "Hello World".

---
+++ OTus_Tetris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OTus_Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
